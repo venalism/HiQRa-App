517af2d6aaa2a29643fc983eb8bc359e5057ee1d
rendykml <rendikamaluddin2018@gmail.com>
2025-07-08 23:52:23 +0700

    masukin cdn tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Aplikasi Absensi QR')</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <script href="https://cdn.jsdelivr.net/npm/tailwindcss@3.3.5/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 
 <body class="bg-gray-100 text-gray-800 flex flex-col min-h-screen">
