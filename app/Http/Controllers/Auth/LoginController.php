1e5f84228668dec43bad4cc4ded01f2e47956ca0
rendykml <rendikamaluddin2018@gmail.com>
2025-07-09 01:10:56 +0700

    masukin logik ketika sudah login tidak bisa kembali ke login form

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+
 class LoginController extends Controller
 {
     /**
@@ -13,6 +14,10 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
+        if (Auth::check()) {
+            return redirect('/');
+            // Atau redirect('/dashboard') jika ingin ke dashboard
+        }
         return view('auth.login');
     }
 
