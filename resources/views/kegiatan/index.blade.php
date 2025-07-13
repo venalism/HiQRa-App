ab1149584871fb526a68c84c9a97906c28768b9c
rendykml <rendikamaluddin2018@gmail.com>
2025-07-13 19:40:07 +0700

    rubah tampilan kegiatan

--- resources/views/kegiatan/index.blade.php
+++ resources/views/kegiatan/index.blade.php
@@ -9,7 +9,12 @@
 @section('content')
     <div class="animate-fadeIn">
         <div class="bg-white rounded-lg shadow-md card-simple p-6">
+
             <div class="flex flex-col sm:flex-row justify-between items-center mb-6">
+                <a href="{{ route('dashboard') }}"
+                    class="px-5 py-2 bg-gray-200 hover:bg-gray-300 text-gray-800 rounded-md font-semibold shadow-md transition">
+                    ← Dashboard
+                </a>
                 <h3 class="text-2xl font-bold text-gray-800 mb-4 sm:mb-0">Daftar Kegiatan</h3>
                 <a href="{{ route('kegiatan.create') }}"
                     class="btn-hover w-full sm:w-auto px-5 py-2 red-gradient text-white rounded-md font-semibold shadow-lg">
@@ -69,7 +74,8 @@
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="text-red-500 hover:text-red-700" title="Hapus">
-                                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                                <svg class="w-5 h-5" fill="none" stroke="currentColor"
+                                                    viewBox="0 0 24 24">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                         d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
                                                     </path>
