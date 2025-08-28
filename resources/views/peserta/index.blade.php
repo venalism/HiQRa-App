36a76e0ff06100b306106c235bd172bed3b43be2
venalism <nadithakbar@gmail.com>
2025-08-28 14:37:48 +0700

    tambah shortcut download

--- resources/views/peserta/index.blade.php
+++ resources/views/peserta/index.blade.php
@@ -105,6 +105,19 @@
                                 <td class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap">{{ $p->kelas->nama ?? '-' }}</td>
                                 <td class="py-4 px-6 text-sm text-center whitespace-nowrap">
                                     <div class="flex justify-center items-center gap-2">
+                                        <a href="{{ route('peserta.qr.download', $p->id) }}"
+                                           title="Download QR Code">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg" data-iconid="software-download"
+                                                data-svgname="Software download">
+                                                <path
+                                                    d="M11 5C11 4.44772 11.4477 4 12 4C12.5523 4 13 4.44772 13 5V12.1578L16.2428 8.91501L17.657 10.3292L12.0001 15.9861L6.34326 10.3292L7.75748 8.91501L11 12.1575V5Z"
+                                                    fill="#000000"></path>
+                                                <path
+                                                    d="M4 14H6V18H18V14H20V18C20 19.1046 19.1046 20 18 20H6C4.89543 20 4 19.1046 4 18V14Z"
+                                                    fill="#000000"></path>
+                                            </svg>
+                                        </a>
                                         <a href="{{ route('peserta.qr', $p->id) }}" class="text-green-600 hover:text-green-900"
                                             title="Tampilkan QR Code">
                                             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
